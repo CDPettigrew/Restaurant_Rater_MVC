2545a72098fa056ea02250007b0682b4e078febe
chris pettigrew <cdpettigrew26@comcast.net>
2022-04-25 10:00:00 -0400

    Changed Titles and basic text

---
+++ Restaurant_Rater_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Restaurant_Rater_MVC.Startup))]
+namespace Restaurant_Rater_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
